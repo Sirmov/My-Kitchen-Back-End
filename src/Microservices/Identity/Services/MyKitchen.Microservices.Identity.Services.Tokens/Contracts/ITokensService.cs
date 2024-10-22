485aa740610abe891944863c3b70837965c91324
Nikola Sirmov <sirmov0213@gmail.com>
2024-10-22 18:29:38 +0300

    Added refresh token functionality and token revocation

--- src/Microservices/Identity/Services/MyKitchen.Microservices.Identity.Services.Tokens/Contracts/ITokensService.cs
+++ src/Microservices/Identity/Services/MyKitchen.Microservices.Identity.Services.Tokens/Contracts/ITokensService.cs
@@ -7,30 +7,42 @@
 
 namespace MyKitchen.Microservices.Identity.Services.Tokens.Contracts
 {
-    using MyKitchen.Microservices.Identity.Data.Models;
+    using MyKitchen.Microservices.Identity.Services.Common.Dtos.User;
+    using MyKitchen.Microservices.Identity.Services.Common.ServiceResult;
 
     /// <summary>
     /// This interface defines the functionality of the tokens service.
     /// </summary>
-    /// <typeparam name="TUser">The type of the identity user.</typeparam>
-    /// <typeparam name="TRole">The type of the identity role.</typeparam>
-    public interface ITokensService<TUser, TRole>
-        where TUser : ApplicationUser, new()
-        where TRole : ApplicationRole, new()
+    public interface ITokensService
     {
         /// <summary>
-        /// This method asynchronously generates a JWT bearer access token based on a identity <paramref name="user"/>.
+        /// This method generates a JWT bearer access token based on a identity <paramref name="user"/>.
         /// </summary>
-        /// <param name="user">The <typeparamref name="TUser"/> containing the user claims.</param>
+        /// <param name="user">The <see cref="UserDto"/> containing the user claims.</param>
         /// <returns>Returns a <see langword="string"/> representation of the access token.</returns>
-        public Task<string> GenerateAccessTokenAsync(TUser user);
+        public ServiceResult<string> GenerateAccessToken(UserDto user);
 
         /// <summary>
         /// This method generates a JWT refresh token based on a identity <paramref name="user"/>
         /// used for generating new access tokens.
         /// </summary>
-        /// <param name="user">The <typeparamref name="TUser"/> containing the user claims.</param>
+        /// <param name="user">The <see cref="UserDto"/> containing the user claims.</param>
         /// <returns>Returns a <see langword="string"/> representation of the refresh token. </returns>
-        public string GenerateRefreshToken(TUser user);
+        public ServiceResult<string> GenerateRefreshToken(UserDto user);
+
+        /// <summary>
+        /// This method asynchronously generates a new access token using a refresh token and the old access token.
+        /// </summary>
+        /// <param name="refreshToken">The <see langword="string"/> representation of the refresh token.</param>
+        /// <param name="accessToken">The <see langword="string"/> representation of the access token.</param>
+        /// <returns>Returns a <see langword="string"/> representation of the new refreshed access token.</returns>
+        public Task<ServiceResult<string>> RefreshAccessTokenAsync(string refreshToken, string accessToken);
+
+        /// <summary>
+        /// This method asynchronously revokes a token.
+        /// </summary>
+        /// <param name="token">The token to be revoked.</param>
+        /// <returns>Returns an empty <see cref="ServiceResult"/>.</returns>
+        public Task<ServiceResult> RevokeTokenAsync(string token);
     }
 }
--- src/Microservices/Identity/Services/MyKitchen.Microservices.Identity.Services.Tokens/TokensService.cs
+++ src/Microservices/Identity/Services/MyKitchen.Microservices.Identity.Services.Tokens/TokensService.cs
@@ -11,48 +11,50 @@ namespace MyKitchen.Microservices.Identity.Services.Tokens
     using System.Security.Claims;
 
     using Microsoft.AspNetCore.DataProtection;
+    using Microsoft.Extensions.Caching.Distributed;
     using Microsoft.Extensions.Options;
     using Microsoft.IdentityModel.Tokens;
 
+    using MyKitchen.Common.Constants;
+    using MyKitchen.Common.ProblemDetails;
     using MyKitchen.Microservices.Identity.Common.Options;
-    using MyKitchen.Microservices.Identity.Data.Models;
+    using MyKitchen.Microservices.Identity.Services.Common.Dtos.User;
+    using MyKitchen.Microservices.Identity.Services.Common.ServiceResult;
     using MyKitchen.Microservices.Identity.Services.Tokens.Contracts;
-    using MyKitchen.Microservices.Identity.Services.Users.Contracts;
 
     /// <summary>
     /// This class contains the business logic around different application tokens.
-    /// It implements <see cref="ITokensService{TUser, TRole}"/>.
+    /// It implements <see cref="ITokensService"/>.
     /// </summary>
-    /// <inheritdoc cref="ITokensService{TUser, TRole}"/>
-    public class TokensService<TUser, TRole> : ITokensService<TUser, TRole>
-        where TUser : ApplicationUser, new()
-        where TRole : ApplicationRole, new()
+    /// <inheritdoc cref="ITokensService"/>
+    public class TokensService : ITokensService
     {
         private readonly JwtSecurityTokenHandler tokenHandler;
+        private readonly TokenValidationParameters tokenValidationParameters;
         private readonly TokenOptions tokenOptions;
         private readonly IDataProtector userIdProtector;
-        private readonly IUserRolesService<TUser, TRole> userRolesService;
+        private readonly IDistributedCache cache;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TokensService{TUser, TRole}"/> class.
         /// </summary>
         /// <param name="tokenOptions">The <see cref="TokenOptions"/>.</param>
         /// <param name="dataProtectionProvider">The implementation of <see cref="IDataProtectionProvider"/>.</param>
-        /// <param name="userRolesService">The implementation of <see cref="IUserRolesService{TUser, TRole}"/>.</param>
+        /// <param name="cache">The implementation of <see cref="IDistributedCache"/>.</param>
         public TokensService(
             IOptions<TokenOptions> tokenOptions,
             IDataProtectionProvider dataProtectionProvider,
-            IUserRolesService<TUser, TRole> userRolesService)
+            IDistributedCache cache)
         {
             this.tokenHandler = new JwtSecurityTokenHandler();
-
             this.tokenOptions = tokenOptions.Value;
+            this.tokenValidationParameters = this.tokenOptions.JwtBearerOptions.TokenValidationParameters;
             this.userIdProtector = dataProtectionProvider.CreateProtector(this.GetType().Namespace!, nameof(this.userIdProtector), "v1");
-            this.userRolesService = userRolesService;
+            this.cache = cache;
         }
 
         /// <inheritdoc/>
-        public async Task<string> GenerateAccessTokenAsync(TUser user)
+        public ServiceResult<string> GenerateAccessToken(UserDto user)
         {
             var authClaims = new List<Claim>
             {
@@ -62,9 +64,7 @@ namespace MyKitchen.Microservices.Identity.Services.Tokens
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             };
 
-            var userRoles = await this.userRolesService.GetUserRolesAsync(user);
-
-            foreach (var role in userRoles.Data ?? Enumerable.Empty<string>())
+            foreach (var role in user.Roles ?? Enumerable.Empty<string>())
             {
                 authClaims.Add(new Claim(ClaimTypes.Role, role));
             }
@@ -73,7 +73,7 @@ namespace MyKitchen.Microservices.Identity.Services.Tokens
         }
 
         /// <inheritdoc/>
-        public string GenerateRefreshToken(TUser user)
+        public ServiceResult<string> GenerateRefreshToken(UserDto user)
         {
             var claims = new List<Claim>
             {
@@ -84,20 +84,73 @@ namespace MyKitchen.Microservices.Identity.Services.Tokens
             return this.GenerateToken(claims, this.tokenOptions.RefreshTokenLifetime);
         }
 
+        /// <inheritdoc/>
+        public async Task<ServiceResult<string>> RefreshAccessTokenAsync(string refreshToken, string accessToken)
+        {
+            var refreshTokenJwt = this.tokenHandler.ReadJwtToken(refreshToken);
+            var accessTokenJwt = this.tokenHandler.ReadJwtToken(accessToken);
+
+            bool isRefreshTokenValid = await this.ValidateTokenAsync(refreshToken);
+            bool isAccessTokenValid = await this.ValidateTokenAsync(accessToken, false);
+            bool isRevoked = (await this.cache.GetAsync(accessTokenJwt.Id)) is not null;
+            bool areUserIdsEqual = refreshTokenJwt.Subject == accessTokenJwt.Subject;
+
+            if ((!isRefreshTokenValid || !isAccessTokenValid) && areUserIdsEqual && !isRevoked)
+            {
+                new UnauthorizedDetails(ExceptionMessages.Unauthorized);
+            }
+
+            var oldAccessTokenJwt = this.tokenHandler.ReadJwtToken(accessToken);
+
+            return this.GenerateToken(oldAccessTokenJwt.Claims, this.tokenOptions.AccessTokenLifetime);
+        }
+
+        /// <inheritdoc/>
+        public async Task<ServiceResult> RevokeTokenAsync(string token)
+        {
+            var tokenJwt = this.tokenHandler.ReadJwtToken(token);
+            var isTokenValid = await this.ValidateTokenAsync(token);
+
+            if (isTokenValid)
+            {
+                DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions()
+                {
+                    AbsoluteExpiration = tokenJwt.ValidTo,
+                };
+
+                await this.cache.SetStringAsync(tokenJwt.Id, string.Empty, cacheOptions);
+            }
+
+            return ServiceResult.Successful;
+        }
+
         private string GenerateToken(IEnumerable<Claim> claims, TimeSpan expiresAfter)
         {
-            var jwtBearerOptions = this.tokenOptions.JwtBearerOptions;
-            var tokenValidationParameters = jwtBearerOptions.TokenValidationParameters;
             SigningCredentials signingCredentials = new (this.tokenOptions.IssuerSigningKey, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
-                issuer: tokenValidationParameters.ValidIssuer,
-                audience: tokenValidationParameters.ValidAudience,
+                issuer: this.tokenValidationParameters.ValidIssuer,
+                audience: this.tokenValidationParameters.ValidAudience,
                 expires: DateTime.Now.Add(expiresAfter),
                 claims: claims,
                 signingCredentials: signingCredentials);
 
             return this.tokenHandler.WriteToken(token);
+        }
+
+        private async Task<bool> ValidateTokenAsync(string token, bool validateLifetime = true)
+        {
+            var jwtToken = this.tokenHandler.ReadJwtToken(token);
+            return await this.ValidateToken(jwtToken, validateLifetime);
+        }
+
+        private async Task<bool> ValidateToken(JwtSecurityToken jwtToken, bool validateLifetime = true)
+        {
+            var validateParameters = this.tokenValidationParameters.Clone();
+            validateParameters.ValidateLifetime = validateLifetime;
+
+            var validationResult = await this.tokenHandler.ValidateTokenAsync(jwtToken, validateParameters);
+            return validationResult.IsValid;
         }
     }
 }
