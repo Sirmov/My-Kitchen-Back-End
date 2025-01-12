34528941c75e673f870d97ec93648eb9b6b9ba9f
Nikola Sirmov <sirmov0213@gmail.com>
2025-01-12 08:22:20 +0200

    Created base controller

---
+++ src/Microservices/Recipes/Api/MyKitchen.Microservices.Recipes.Api.Rest/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+// |-----------------------------------------------------------------------------------------------------|
+// <copyright file="BaseController.cs" company="MyKitchen">
+// Copyright (c) MyKitchen. All Rights Reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+// |-----------------------------------------------------------------------------------------------------|
+
+namespace MyKitchen.Microservices.Recipes.Api.Rest.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// This class is a base api controller for all controllers.
+    /// It inherits the <see cref="ControllerBase"/>.
+    /// </summary>
+    [ApiController]
+    [Authorize]
+    public class BaseController : ControllerBase
+    {
+    }
+}
