f4a5b3a43eb4f9c94c1f8ff33bb8695198b2c2c2
Nikola Sirmov <sirmov0213@gmail.com>
2025-01-12 08:20:50 +0200

    Created application extensions

---
+++ src/Microservices/Recipes/Api/MyKitchen.Microservices.Recipes.Api.Rest/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,57 @@
+// |-----------------------------------------------------------------------------------------------------|
+// <copyright file="ApplicationExtensions.cs" company="MyKitchen">
+// Copyright (c) MyKitchen. All Rights Reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+// |-----------------------------------------------------------------------------------------------------|
+
+namespace MyKitchen.Microservices.Recipes.Api.Rest.Extensions
+{
+    using MyKitchen.Microservices.Recipes.Api.Rest.Options;
+    using MyKitchen.Microservices.Recipes.Services.Recipes;
+    using MyKitchen.Microservices.Recipes.Services.Recipes.Contracts;
+
+    /// <summary>
+    /// This static class contains <see cref="IServiceCollection"/> extension methods used for
+    /// registering application dependencies.
+    /// </summary>
+    public static class ApplicationExtensions
+    {
+        /// <summary>
+        /// This method registers all application options configuration.
+        /// </summary>
+        /// <param name="services">The service collection where the application options should be registered.</param>
+        /// <returns>Returns the service collection with all application options registered.</returns>
+        public static IServiceCollection AddApplicationOptions(this IServiceCollection services)
+        {
+            services
+                .AddOptionsWithValidateOnStart<MongoDbOptions>()
+                .BindConfiguration(nameof(MongoDbOptions))
+                .ValidateDataAnnotations();
+
+            return services;
+        }
+
+        /// <summary>
+        /// This method registers all application services.
+        /// </summary>
+        /// <param name="services">The service collection where the application services should be registered.</param>
+        /// <returns>Returns the service collection with all application services registered.</returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRecipesService), typeof(RecipesService));
+
+            return services;
+        }
+
+        /// <summary>
+        /// This method registers all application middlewares.
+        /// </summary>
+        /// <param name="services">The service collection where the application middlewares should be registered.</param>
+        /// <returns>Returns the service collection with all application middlewares registered.</returns>
+        public static IServiceCollection AddApplicationMiddlewares(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
