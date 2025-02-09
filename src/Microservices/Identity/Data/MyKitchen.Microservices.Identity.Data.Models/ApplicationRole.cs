86f96da1681de82298f15771055a71ad8af25676
Nikola Sirmov <sirmov0213@gmail.com>
2025-02-09 13:01:00 +0200

    Genere id on creation

--- src/Microservices/Identity/Data/MyKitchen.Microservices.Identity.Data.Models/ApplicationRole.cs
+++ src/Microservices/Identity/Data/MyKitchen.Microservices.Identity.Data.Models/ApplicationRole.cs
@@ -9,10 +9,25 @@ namespace MyKitchen.Microservices.Identity.Data.Models
 {
     using AspNetCore.Identity.Mongo.Model;
 
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
     /// <summary>
     /// This class is a extension of the base mongo identity role class. It inherits <see cref="MongoRole{TKey}"/>.
     /// </summary>
     public class ApplicationRole : MongoRole<string>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationRole"/> class.
+        /// </summary>
+        public ApplicationRole()
+        {
+            this.Id = ObjectId.GenerateNewId().ToString();
+        }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public override string Id { get => base.Id; set => base.Id = value; }
     }
 }
--- src/Microservices/Identity/Data/MyKitchen.Microservices.Identity.Data.Models/ApplicationUser.cs
+++ src/Microservices/Identity/Data/MyKitchen.Microservices.Identity.Data.Models/ApplicationUser.cs
@@ -9,10 +9,25 @@ namespace MyKitchen.Microservices.Identity.Data.Models
 {
     using AspNetCore.Identity.Mongo.Model;
 
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
     /// <summary>
     /// This class is a extension of the base mongo identity user class. It inherits <see cref="MongoUser{TKey}"/>.
     /// </summary>
     public class ApplicationUser : MongoUser<string>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationUser"/> class.
+        /// </summary>
+        public ApplicationUser()
+        {
+            this.Id = ObjectId.GenerateNewId().ToString();
+        }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public override string Id { get => base.Id; set => base.Id = value; }
     }
 }
